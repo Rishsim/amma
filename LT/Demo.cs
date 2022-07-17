7fcd1fcef7428e5b9c0337f336312bbcb97d537b
LAPTOP-EFSKR9KV\ramya <ramya0502@gmail.com>
2022-07-17 22:49:45 +0100

    Lt method2

---
+++ LT/Demo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LT
+{
+    public class Demo
+    {
+        public void B1()
+        {
+            List<int> a = new List<int>();
+
+            a.Add(10);
+
+            a.Add(20);
+
+            foreach (var c in a)
+                
+            {
+                Console.WriteLine(c);
+                    
+                    
+                    }
+        }
+        
+
+
+
+    }
+}
