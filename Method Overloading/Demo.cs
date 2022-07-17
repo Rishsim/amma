419b88aa202979a89c0a3b05255e709eb5891818
LAPTOP-EFSKR9KV\ramya <ramya0502@gmail.com>
2022-07-17 22:45:22 +0100

    method overload added

---
+++ Method Overloading/Demo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    public class Demo
+    {
+        public void z1()
+        {
+            Console.WriteLine("this is my first method z1" );
+        }
+        public void z1(int i)
+        {
+            Console.WriteLine("this is my second method z1");
+            Console.WriteLine("this is intvalue"+ i);
+        }
+        public void z1(string j)
+        {
+            Console.WriteLine("this is my third method z1");
+        }
+
+        public void z1(int i,string j,int k)
+        {
+            Console.WriteLine("this is my fourth method z1");
+        }
+        
+
+
+    }
+}
---
+++ Method Overloading/Sample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    internal class Sample
+    {
+        static void Main(string[] args)
+        {
+            Demo s = new Demo();
+
+            s.z1(10,"ramya",20);
+            s.z1(5);
+
+        }
+    }
+}
